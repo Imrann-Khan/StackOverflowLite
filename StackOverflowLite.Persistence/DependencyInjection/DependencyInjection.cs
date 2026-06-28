d3f0b8fdce66761aeae0c53b002830fe528ad141
Imrann-Khan <ik11150500218@gmail.com>
2026-06-29 01:56:57 +0600

    finished all the criterias

--- StackOverflowLite.Persistence/DependencyInjection/DependencyInjection.cs
+++ StackOverflowLite.Persistence/DependencyInjection/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackOverflowLite.Application.Services.Interfaces;
 using StackOverflowLite.Persistence.Data;
 
 namespace StackOverflowLite.Persistence.DependencyInjection;
@@ -11,7 +12,7 @@ public static class DependencyInjection
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-        services.AddScoped<StackOverflowLite.Application.Services.Interfaces.IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+        services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
 
         return services;
     }
